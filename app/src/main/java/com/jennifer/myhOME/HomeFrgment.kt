ddba0d4e3697db7830111f1311e01ec834e6fd9a
jenny6823 <dibiejennifer@gmail.com>
2021-07-14 14:38:11 +0100

    Initial commit

---
+++ app/src/main/java/com/jennifer/myhOME/HomeFrgment.kt
@@ -0,0 +1,25 @@
+package com.jennifer.myhOME
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFrgment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+      
+
+    }
+}
